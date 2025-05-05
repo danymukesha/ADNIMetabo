e2380e62b058892e8e14d8fcad4ec021b5d546b9
danymukesha <danymukesha@gmail.com>
2025-05-05 22:41:51 +0200

    updates and started normalization

---
+++ data-raw/04.v01_data_metabo.R
@@ -0,0 +1,273 @@
+library(data.table)
+library(openxlsx)
+library(dplyr)
+library(tidyr)
+library(tibble)
+library(janitor)
+
+transpose_df <- function(df) {
+    t_df <- data.table::transpose(df)
+    colnames(t_df) <- rownames(df)
+    rownames(t_df) <- colnames(df)
+    t_df <- t_df %>%
+        tibble::rownames_to_column(.data = .) %>%
+        tibble::as_tibble(.)
+    return(t_df)
+}
+
+# QC data ====
+## FIA ====
+FIA_QC <-  openxlsx::read.xlsx(
+    xlsxFile = "data-raw/ADMC_DUKE_Q500_FIA_QC_20230522.xlsx",
+    skipEmptyRows = TRUE, sheet = 'ADMC_DUKE_Q500_FIA_QC') |>
+    tibble::tibble()
+
+FIA_LODs <- FIA_QC |>
+    dplyr::slice(1:8) |>
+    dplyr::select_if( ~ !any(is.na(.))) |>
+    transpose_df() |>
+    janitor::row_to_names(row_number = 1) |>
+    dplyr::rename("Short.Name/Injection.Number" = "Injection.Number")
+
+FIA_QC_samples <-  FIA_QC |>
+    dplyr::slice(9:n()) |>
+    dplyr::select(-c(Customer.Sample.Identification,
+                     Sample.Bar.Code, Sample.Type,
+                     Sample.Identification, Species,
+                     Material, Well.Position,
+                     Sample.Volume, Run.Number,
+                     Injection.Number))
+
+K <- FIA_QC_samples |>
+    ADNIMetabo:::process_data_with_report(LODs = FIA_LODs,
+                             inj_type = "FIA",
+                             qc_samples = TRUE)
+
+FIA_QC_samples <- K$processed_data
+K$report$removed_columns
+K$report$imputed_columns
+rm(K)
+
+## UPLC ====
+UPLC_QC <-  openxlsx::read.xlsx(
+    xlsxFile = "data-raw/ADMC_DUKE_Q500_UPLC_QC_20230522.xlsx",
+    skipEmptyRows = TRUE, sheet = 'ADMC_DUKE_Q500_UPLC_QC') |>
+    tibble::tibble()
+
+UPLC_LODs <- UPLC_QC |>
+    dplyr::slice(1:5) |>
+    dplyr::select_if(~ !any(is.na(.))) |>
+    transpose_df() |>
+    janitor::row_to_names(row_number = 1) |>
+    dplyr::rename("Short.Name/Injection.Number" = "Injection.Number")
+
+UPLC_QC_samples <- UPLC_QC |>
+    dplyr::slice(6:n()) |>
+    dplyr::select(-c(Customer.Sample.Identification,
+                     Sample.Bar.Code, Sample.Type,
+                     Sample.Identification, Species,
+                     Material, Well.Position,
+                     Sample.Volume, Run.Number,
+                     Injection.Number, ))
+
+K <- UPLC_QC_samples |>
+    ADNIMetabo:::process_data_with_report(LODs = UPLC_LODs,
+                             inj_type = "UPLC",
+                             qc_samples = TRUE)
+
+UPLC_QC_samples <- K$processed_data |>
+    mutate_at(vars(-c(Plate.Bar.Code)), as.numeric)
+K$report$removed_columns
+K$report$imputed_columns
+rm(K)
+
+# Metabo data ====
+## FIA ====
+FIA_Metabo <- data.table::fread(
+    input = "data-raw/ADMC_DUKE_Q500_FIA_11Nov2024.csv",
+    sep = ",") |> tibble::tibble()
+
+FIA_Metabo <- FIA_Metabo |>
+    dplyr::select(-c(`Customer Sample Identification`,
+                     `Sample Bar Code`, # `Sample Type`,
+                     `Sample Identification`, Species,
+                     Material, `Well Position`,
+                     `Sample Volume`, `Run Number`,
+                     `Injection Number`,
+                     EXAMDATE, #`Plate Bar Code`,
+                     update_stamp))
+
+FIA_processed <- FIA_Metabo |>
+    dplyr::rename(VISCODE = VISCODE2) |>
+    ## dplyr::filter(VISCODE == "bl") |>
+    ## add_metadata(other_info = adnimerge) |>
+    ADNIMetabo:::process_data_with_report(LODs = FIA_LODs, inj_type = "FIA")
+
+## UPLC ====
+UPLC_Metabo <- data.table::fread(
+    input = "data-raw/ADMC_DUKE_Q500_UPLC_11Nov2024.csv",
+    sep = ",") |> tibble::tibble()
+
+UPLC_Metabo <- UPLC_Metabo |>
+    dplyr::select(-c(`Customer Sample Identification`,
+                     `Sample Bar Code`, #`Sample Type`,
+                     `Sample Identification`, Species,
+                     Material, `Well Position`,
+                     `Sample Volume`, `Run Number`,
+                     `Injection Number`,
+                     EXAMDATE, # `Plate Bar Code`,
+                     update_stamp))
+
+UPLC_processed <- UPLC_Metabo |>
+    dplyr::rename(VISCODE = VISCODE2) |> # renamed for f(x) ADNIMetabo:::add_metadata
+    # dplyr::filter(VISCODE == "bl") |>
+    ## add_metadata(other_info = adnimerge) |>
+    ADNIMetabo:::process_data_with_report(LODs = UPLC_LODs, inj_type = "UPLC")
+
+
+# Merge QCs with Metabo ====
+## FIA ====
+FIA_QC_samples <- FIA_QC_samples |>
+    # summarise(across(where(is.numeric), ~ mean(.x, na.rm = TRUE))) |>
+    dplyr::mutate(`RID` = 0, .before = Plate.Bar.Code) |>
+    dplyr::mutate(`VISCODE` = "-", .before = Plate.Bar.Code) |>
+    dplyr::mutate(`Sample Type` = "QC", .after = Plate.Bar.Code)
+
+# replace space by point in metabolite names
+colnames(FIA_QC_samples) <-
+    gsub("\\.", " ", FIA_QC_samples |> colnames())
+
+commcols <- intersect(names(FIA_QC_samples),
+                      names(FIA_processed$processed_data))
+
+FIA_processed$qc_w_metabo_data <- bind_rows( # lst()
+    select(FIA_QC_samples, all_of(commcols)),
+    select(FIA_processed$processed_data, all_of(commcols))
+    )
+
+## UPLC ====
+UPLC_QC_samples <- UPLC_QC_samples |>
+    # summarise(across(where(is.numeric), ~ mean(.x, na.rm = TRUE))) |>
+    dplyr::mutate(`RID` = 0, .before = Trigonelline) |>
+    dplyr::mutate(`VISCODE` = "-", .before = Trigonelline) |>
+    dplyr::mutate(`Sample Type` = "QC", .before = Trigonelline) # `Trigonelline`: first metabolite in column
+
+# replace space by point in metabolite names
+colnames(UPLC_QC_samples) <-
+    gsub("\\.", " ", UPLC_QC_samples |> colnames())
+
+commcols <- intersect(names(UPLC_QC_samples),
+                      names(UPLC_processed$processed_data))
+
+UPLC_processed$qc_w_metabo_data <- bind_rows( # lst()
+    select(UPLC_QC_samples, all_of(commcols)),
+    select(UPLC_processed$processed_data, all_of(commcols))
+)
+
+
+rm("FIA_QC", "UPLC_QC")
+
+# Save the data sets ====
+usethis::use_data(FIA_LODs, overwrite = TRUE)
+usethis::use_data(FIA_QC_samples, overwrite = TRUE)
+usethis::use_data(FIA_Metabo, overwrite = TRUE)
+usethis::use_data(UPLC_LODs, overwrite = TRUE)
+usethis::use_data(UPLC_QC_samples, overwrite = TRUE)
+usethis::use_data(UPLC_Metabo, overwrite = TRUE)
+
+usethis::use_data(UPLC_processed, overwrite = TRUE)
+usethis::use_data(FIA_processed, overwrite = TRUE)
+
+
+## todo: recheck well the above code, but everything should be good.
+
+All_Metabo <- UPLC_processed$qc_w_metabo_data |> dplyr::full_join(
+    FIA_processed$qc_w_metabo_data, by = c("RID" = "RID", "VISCODE" = "VISCODE"))
+
+# let's load a collection of key metrics and observations for all ADNI participants
+adnimerge <-  data.table::fread(
+    input = "data-raw/ADNIMERGE_20Sep2024.csv",
+    sep = ",") |> tibble::tibble() |>
+    dplyr::filter(VISCODE == "bl")
+
+All_metabo <- All_Metabo |>
+    ADNIMetabo:::add_metadata(other_info = adnimerge)
+
+
+# PCA for FIA raw ====
+
+# replace space by point in metabolite names
+colnames(FIA_Metabo) <-
+    gsub(" ", ".", FIA_Metabo |> colnames())
+
+raw_data <- FIA_processed$processed_data |>
+    cbind(UPLC_processed$processed_data[-c(1:9)]) |>
+    data.frame()
+
+raw_data1 <- raw_data[-c(1:9)] |>
+    dplyr::mutate_at(colnames(raw_data[-c(1:9)]), as.numeric)
+raw_data1 <- raw_data1[sapply(raw_data1, is.numeric)]
+
+dplyr::bind_rows(raw_data1, cbind(FIA_QC_samples |> dplyr::select(-1),
+                         UPLC_QC_samples |> dplyr::select(-1)))
+
+mypca <-  prcomp(raw_data1)
+pca <- cbind(raw_data[1:9], mypca$x[], 1:2)
+ggplot(pca, aes(PC1,PC2, col = DX_bl, fill = DX_bl)) +
+    stat_ellipse(geom = "polygon", col = "black", alpha = 0.5) +
+    geom_point(shape = 21, col = "black")
+
+
+biomarkers <- raw_data[1:9]
+acp <- PCA(raw_data1, graph = FALSE)
+pca_coords <- as.data.frame(acp$ind$coord[, 1:5])
+colnames(pca_coords) <- paste0("Dim", 1:5)
+
+pca_coords$Cohort <- as.factor(raw_data$DX_bl)
+pca_coords$sampleID <- raw_data$RID
+
+coul <- rainbow(length(unique(pca_coords$Cohort)))
+color_map <- setNames(coul, levels(pca_coords$Cohort))
+pca_coords$Color <- color_map[pca_coords$Cohort]
+
+axis = list(showline = FALSE, zeroline = FALSE, gridcolor = '#ffff',
+            ticklen = 4, titlefont = list(size = 13))
+
+
+library(plotly)
+splom_plot <- plot_ly(
+    data = pca_coords,
+    type = 'splom',
+    dimensions = list(
+        list(label = paste("PC1: (", round(acp$eig[1, 2], 2), "%)", sep = ""), values = ~Dim1),
+        list(label = paste("PC2: (", round(acp$eig[2, 2], 2), "%)", sep = ""), values = ~Dim2),
+        list(label = paste("PC3: (", round(acp$eig[3, 2], 2), "%)", sep = ""), values = ~Dim3),
+        list(label = paste("PC4: (", round(acp$eig[4, 2], 2), "%)", sep = ""), values = ~Dim4)
+    ),
+    color = ~pca_coords$Cohort, colors = c('#636EFA','#EF553B'),
+    text = ~paste("sample: ", sampleID)
+) %>%
+    layout(
+        title = "Scatterplot Matrix of PCA 4 Dimensions",
+        dragmode = "select",
+        legend = list(title = list(text = 'Group')),
+        hovermode = 'closest',
+        dragmode = 'select',
+        plot_bgcolor = 'rgba(240,240,240, 0.95)',
+        xaxis = list(domain = NULL, showline = F, zeroline = F, gridcolor = '#ffff', ticklen = 4),
+        yaxis = list(domain = NULL, showline = F, zeroline = F, gridcolor = '#ffff', ticklen = 4),
+        xaxis2 = axis,
+        xaxis3 = axis,
+        xaxis4 = axis,
+        yaxis2 = axis,
+        yaxis3 = axis,
+        yaxis4 = axis
+    )  %>% style(diagonal = list(visible = FALSE))
+
+splom_plot
+
+
+
+
+
+
--- data-raw/05_normalization.R
+++ data-raw/05_normalization.R
@@ -18,40 +18,54 @@ library(tidyverse)
 #   QC_Level = "QC2",
 #   TargetValue = c(1.3, 2.1, ...)
 # )
+# # STEP 1: Preprocessing ====
+# valid_data <- trans |>
+#    dplyr::filter(Concentration > LOD_threshold, !is.na(Concentration))
 
-tbl <- FIA_processed$qc_w_metabo_data
+tbbl <- FIA_processed$qc_w_metabo_data
 # todo: now FIA for test, then will create function
 # to automatized the process with but FIA and UPLC
 
-data <- tbl |>
-    tidyr::separate(`Plate Bar Code`, into = c("Plate", "SampleNumber"), sep = "-") %>%
-    dplyr::rename(SampleType = `Sample Type`) %>%
-    mutate(SampleID = paste0(SampleType, "_", SampleNumber)) %>%
+# check out the duplicated RID - VISCODE
+tbbl |> dplyr::filter(`Sample Type` != "QC") |>
+    dplyr::group_by(RID, VISCODE, `Plate Bar Code`) |>
+    dplyr::filter(n() > 1 ) |>
+    dplyr::summarize(n = n())
 
-    pivot_longer(
-        cols = -c(RID, VISCODE, Plate, SampleType, SampleNumber, SampleID),
+#todo: remove the duplicated rows
+
+transformed_data <- tbbl |>
+    tidyr::separate(`Plate Bar Code`,
+                    into = c("Plate", "PlateID"),
+                    sep = "-") |>
+    dplyr::select(-PlateID) |>
+    dplyr::mutate(SampleID = paste0(RID, "_", VISCODE)) |>
+    tidyr::pivot_longer(
+        cols = -c(RID, VISCODE, Plate, `Sample Type`, SampleID),
         names_to = "Analyte",
         values_to = "Concentration"
-    ) %>%
-    select(Plate, SampleID, SampleType, Analyte, Concentration)
+    )
+    #dplyr::select(Plate, SampleID, SampleType, Analyte, Concentration)
 
 
-target_values <- tbl %>%
-    filter(`Sample Type` == "QC") %>%
-    select(-RID, -VISCODE, -`Plate Bar Code`, -`Sample Type`) %>%
-    summarise(across(everything(), mean, na.rm = TRUE)) %>%
+target_values <- tbbl |>
+    dplyr::filter(`Sample Type` == "QC") |>
+    dplyr::select(-RID, -VISCODE, -`Plate Bar Code`, -`Sample Type`) |>
+    summarise(across(everything(), mean, na.rm = TRUE)) |>
 
-    pivot_longer(
+    tidyr::pivot_longer(
         cols = everything(),
         names_to = "Analyte",
         values_to = "TargetValue"
-    ) %>%
+    ) |>
 
-    mutate(QC_Level = "QC") %>%
-    select(Analyte, QC_Level, TargetValue)
+    dplyr::mutate(QC_Level = "QC") |>
+    dplyr::select(Analyte, QC_Level, TargetValue)
 
 head(target_values)
 head(transformed_data)
+
+valid_data <- transformed_data # apply assertion if needed for TODO.
 
 # CONFIGURATION ====
 LOD_threshold <- 0.0001
@@ -59,45 +73,104 @@ use_median <- TRUE
 normalization_type <- "target" # or "reference"
 reference_sample <- "QC" # can be a sample name or sample type
 
-# STEP 1: Preprocessing ====
-valid_data <- data %>%
-    dplyr::filter(Concentration > LOD_threshold, !is.na(Concentration))
-
 # STEP 2: Calculate correction factors ====
 if (normalization_type == "target") {
 
-    correction_factors <- valid_data %>%
-        filter(SampleType == "QC", grepl(reference_sample, SampleID)) %>%
-        group_by(Plate, Analyte) %>%
-        summarize(Stat = if (use_median) median(Concentration) else mean(Concentration), .groups = "drop") %>%
-        left_join(target_values, by = "Analyte") %>%
-        mutate(CorrectionFactor = Stat / TargetValue)
+    correction_factors <- valid_data |>
+        dplyr::filter(`Sample Type` == "QC", grepl(reference_sample, `Sample Type`)) |>
+        dplyr::group_by(Plate, Analyte) |>
+        dplyr::summarize(
+            Stat = if (use_median)
+                median(Concentration)
+            else
+                mean(Concentration),
+            .groups = "drop"
+        ) |>
+        dplyr::left_join(target_values, by = "Analyte") |>
+        dplyr::mutate(CorrectionFactor = Stat / TargetValue)
 
 } else if (normalization_type == "reference") {
 
-    # step A: calculate A = mean/median per plate, per analyte
-    A <- valid_data %>%
-        filter(SampleID == reference_sample | SampleType == reference_sample) %>%
-        group_by(Plate, Analyte) %>%
-        summarize(A = if (use_median) median(Concentration) else mean(Concentration), .groups = "drop")
+    # step A: calculate A = mean/median per plate, per analyte/metabolite
+    A <- valid_data |>
+        dplyr::filter(SampleID == reference_sample | SampleType == reference_sample) |>
+        dplyr::group_by(Plate, Analyte) |>
+        dplyr::summarize(A = if (use_median)
+            median(Concentration)
+            else
+                mean(Concentration),
+            .groups = "drop")
 
-    # step B: calculate B = overall mean/median per analyte
-    B <- A %>%
-        group_by(Analyte) %>%
-        summarize(B = if (use_median) median(A) else mean(A), .groups = "drop")
+    # step B: calculate B = overall mean/median per analyte/metabolite
+    B <- A |>
+        dplyr::group_by(Analyte) |>
+        dplyr::summarize(B = if (use_median)
+            median(A)
+            else
+                mean(A), .groups = "drop")
 
     # step C: correction factor = A / B
-    correction_factors <- A %>%
-        left_join(B, by = "Analyte") %>%
-        mutate(CorrectionFactor = A / B)
+    correction_factors <- A |>
+        dplyr::left_join(B, by = "Analyte") |>
+        dplyr::mutate(CorrectionFactor = A / B)
 }
 
 # STEP 3: Normalize concentrations ====
-normalized_data <- valid_data %>%
-    left_join(correction_factors, by = c("Plate", "Analyte")) %>%
-    mutate(NormalizedConcentration = Concentration / CorrectionFactor)
+normalized_data <- valid_data |>
+    dplyr::left_join(correction_factors, by = c("Plate", "Analyte")) |>
+    dplyr::mutate(NormalizedConcentration = Concentration / CorrectionFactor) |>
+    dplyr::select(-c(Stat, QC_Level, TargetValue))
+
+# for Samples without their correspoonding plate for QCs
+correction_factors_mean <- correction_factors |>
+    dplyr::select(-c(Plate)) |>
+    dplyr::group_by(Analyte) |>
+    dplyr::summarize(
+        mean = mean(CorrectionFactor, na.rm = TRUE),
+        .groups = "drop"
+    ) |>
+    dplyr::rename(CorrectionFactor = mean)
+
+normalized_data_mean <- normalized_data |> filter(if_any(everything(), ~ is.na(.))) |>
+    dplyr::select(where(function(x) !all(is.na(x)))) |>
+    dplyr::left_join(correction_factors_mean, by = c("Analyte")) |>
+    dplyr::mutate(NormalizedConcentration = Concentration / CorrectionFactor)
+
+normalized_data <- normalized_data |> filter(if_all(everything(), ~ !is.na(.))) |>
+    dplyr::bind_rows(normalized_data_mean) |>
+    dplyr::filter(`Sample Type` != "QC")
 
 # result ====
 head(normalized_data)
 
-## todo: reformate the normalized data into the original format `tbl`.
+## toreview: reformate the normalized data into the original format `tbbl`.
+reformatted_normalized_data <- normalized_data %>%
+    # extract `SampleNumber` from SampleID (e.g., "QC_1" -> "1")
+    dplyr::mutate(
+        SampleNumber = stringr::str_extract(SampleID, "\\d+$"),
+        `Plate Bar Code` = paste(Plate, SampleNumber, sep = "-")
+    ) |>
+    # Rename SampleType to match original column name
+    dplyr::rename(`Sample Type` = SampleType) %>%
+    # Select and order columns to match original tbbl structure
+    dplyr::select(
+        `Plate Bar Code`,
+        `Sample Type`,
+        Analyte,
+        NormalizedConcentration
+    ) %>%
+    # Pivot back to wide format with analytes as columns
+    tidyr::pivot_wider(
+        names_from = Analyte,
+        values_from = NormalizedConcentration
+    ) %>%
+    # Ensure the order matches the original tbbl (add other columns if needed)
+    dplyr::select(`Plate Bar Code`, `Sample Type`, everything())
+
+normalized_data |>
+    dplyr::select(RID, VISCODE, Plate, `Sample Type`,
+                  Analyte, NormalizedConcentration)
+
+
+# View the reformatted data
+head(reformatted_normalized_data)
